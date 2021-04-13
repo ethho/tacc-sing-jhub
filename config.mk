59301540abfdb84d96eb14b3c86b324bd8f6e426
Ethan Ho <eho@tacc.utexas.edu>
2021-04-13 16:27:58 -0600

    Nicks defaults

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 ALLOCATION ?= SD2E-Community # allocation for sbatch
-PARTITION ?= development # partition (AKA queue) for sbatch
+PARTITION ?= gtx # partition (AKA queue) for sbatch
 DOTENV ?= ./dotenv/work2cache.env # environment variables set outside container
 IMAGE ?= sd2e/tacc-ml-jupyter:centos7-cuda10-tf2.2-pt1.7 # Docker image
-SIF ?= ./my_image.sif # path to which Docker image will be pulled
+SIF ?= ./tacc-ml.sif # path to which Docker image will be pulled

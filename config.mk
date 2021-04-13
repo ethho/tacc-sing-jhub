f0030d5fca6af30e12789cf266845b05e4ad4cfa
eho-tacc <eho@tacc.utexas.edu>
2021-04-13 16:43:17 -0500

    Correct SIF path

--- config.mk
+++ config.mk
@@ -2,4 +2,4 @@ DOTENV ?= ./dotenv/work2cache.env
 IMAGE ?= sd2e/tacc-ml-jupyter:centos7-cuda10-tf2.2-pt1.7
 ALLOCATION ?= SD2E-Community
 CONDA_ENV ?= envs/tf2_2.yaml
-SIF ?= ./tacc-ml.sif
+SIF ?= ./my_image.sif

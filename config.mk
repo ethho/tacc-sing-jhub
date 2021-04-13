d0cd9523da6bd60f02b0a531f13fd19718de5a80
Ethan Ho <eho@tacc.utexas.edu>
2021-04-13 13:47:48 -0600

    Move dotenvs to separate dir

--- config.mk
+++ config.mk
@@ -1,5 +1,4 @@
-DOTENV ?= ./work2cache.env
-PYTHON ?= python3
+DOTENV ?= ./dotenv/work2cache.env
 IMAGE ?= sd2e/tacc-ml-jupyter:centos7-cuda10-tf2.2-pt1.7
 ALLOCATION ?= SD2E-Community
 CONDA_ENV ?= envs/tf2_2.yaml
